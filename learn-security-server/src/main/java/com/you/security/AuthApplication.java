6c16754220b376176a5dd85e00c2b178d2cf9402
fugui <aa>
2020-02-18 17:39:02 +0800

    security auth leaning

---
+++ learn-security-server/src/main/java/com/you/security/AuthApplication.java
@@ -0,0 +1,21 @@
+package com.you.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <一句话说明功能>
+ * <功能详细描述>
+ *
+ * @author admin
+ * @title <一句话说明功能>
+ * @date 2020/2/18 17:36
+ * @since <版本号>
+ */
+@SpringBootApplication
+public class AuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthApplication.class);
+    }
+}
